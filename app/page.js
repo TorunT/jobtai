9f370c5b2524e28bfe9501345bfd1bff382048d5
TULAY TORUN <72720975+TorunT@users.noreply.github.com>
2026-07-14 00:15:35 -0400

    Replace redirect with HTML meta refresh

--- app/page.js
+++ app/page.js
@@ -1,5 +1,10 @@
-import { redirect } from 'next/navigation';
-
 export default function Home() {
-  redirect('/app.html');
+  return (
+    <html>
+      <head>
+        <meta httpEquiv="refresh" content="0; url=/app.html" />
+      </head>
+      <body></body>
+    </html>
+  );
 }
