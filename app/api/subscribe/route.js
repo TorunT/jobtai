2b4e149a0a94ae7ef1754e8c566b937273eff181
TULAY TORUN <72720975+TorunT@users.noreply.github.com>
2026-07-13 23:36:04 -0400

    Update route.js

--- app/api/subscribe/route.js
+++ app/api/subscribe/route.js
@@ -1,4 +1,5 @@
-import { supabase } from '../../lib/supabase';
+import { createClient } from '@supabase/supabase-js';
+const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
 
 export async function POST(request) {
   try {
